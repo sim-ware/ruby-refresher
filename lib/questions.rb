709b1ef95377a5059e83a0c50b332773d3b9e188
Samir Gossain <samir.gossain@gmail.com>
2017-02-18 16:58:23 +0000

    9 Tests Passing

--- lib/questions.rb
+++ lib/questions.rb
@@ -80,10 +80,12 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.min_by(&:length)
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.max_by(&:length)
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
